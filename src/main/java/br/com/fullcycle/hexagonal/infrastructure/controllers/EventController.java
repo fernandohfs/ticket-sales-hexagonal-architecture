e1ece42dbccb5d94817452b14fc60217d5271483
Fernando Soares <fernando.hfs94@gmail.com>
2024-08-06 20:57:08 -0300

    refactor(dto): improve to use record class

--- src/main/java/br/com/fullcycle/hexagonal/infrastructure/controllers/EventController.java
+++ src/main/java/br/com/fullcycle/hexagonal/infrastructure/controllers/EventController.java
@@ -3,7 +3,7 @@ package br.com.fullcycle.hexagonal.infrastructure.controllers;
 import br.com.fullcycle.hexagonal.application.exceptions.ValidationException;
 import br.com.fullcycle.hexagonal.application.usecases.CreateEventUseCase;
 import br.com.fullcycle.hexagonal.application.usecases.SubscribeCustomerToEventUseCase;
-import br.com.fullcycle.hexagonal.infrastructure.dtos.EventDTO;
+import br.com.fullcycle.hexagonal.infrastructure.dtos.NewEventDTO;
 import br.com.fullcycle.hexagonal.infrastructure.dtos.SubscribeDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,10 +32,9 @@ public class EventController {
 
     @PostMapping
     @ResponseStatus(CREATED)
-    public ResponseEntity<?> create(@RequestBody EventDTO dto) {
+    public ResponseEntity<?> create(@RequestBody NewEventDTO dto) {
         try {
-            final var partnerId = Objects.requireNonNull(dto.getPartner(), "Partner is required").getId();
-            final var output = createEventUseCase.execute(new CreateEventUseCase.Input(dto.getDate(), dto.getName(), partnerId, dto.getTotalSpots()));
+            final var output = createEventUseCase.execute(new CreateEventUseCase.Input(dto.date(), dto.name(), dto.partnerId(), dto.totalSpots()));
             return ResponseEntity.created(URI.create("/events/" + output.id())).body(output);
         } catch (ValidationException ex) {
             return ResponseEntity.unprocessableEntity().body(ex.getMessage());
@@ -47,7 +46,7 @@ public class EventController {
     public ResponseEntity<?> subscribe(@PathVariable Long id, @RequestBody SubscribeDTO dto) {
         try {
             final var output =
-                    subscribeCustomerToEventUseCase.execute(new SubscribeCustomerToEventUseCase.Input(id, dto.getCustomerId()));
+                    subscribeCustomerToEventUseCase.execute(new SubscribeCustomerToEventUseCase.Input(id, dto.customerId()));
             return ResponseEntity.ok(output);
         } catch (ValidationException ex) {
             return ResponseEntity.unprocessableEntity().body(ex.getMessage());
--- src/test/java/br/com/fullcycle/hexagonal/infrastructure/controllers/EventControllerTest.java
+++ src/test/java/br/com/fullcycle/hexagonal/infrastructure/controllers/EventControllerTest.java
@@ -1,7 +1,7 @@
 package br.com.fullcycle.hexagonal.infrastructure.controllers;
 
-import br.com.fullcycle.hexagonal.infrastructure.dtos.EventDTO;
-import br.com.fullcycle.hexagonal.infrastructure.dtos.PartnerDTO;
+import br.com.fullcycle.hexagonal.application.usecases.CreateCustomerUseCase;
+import br.com.fullcycle.hexagonal.infrastructure.dtos.NewEventDTO;
 import br.com.fullcycle.hexagonal.infrastructure.dtos.SubscribeDTO;
 import br.com.fullcycle.hexagonal.infrastructure.models.Customer;
 import br.com.fullcycle.hexagonal.infrastructure.models.Partner;
@@ -59,12 +59,7 @@ class EventControllerTest {
     @Test
     @DisplayName("Deve criar um evento")
     public void testCreate() throws Exception {
-
-        var event = new EventDTO();
-        event.setDate("2021-01-01");
-        event.setName("Disney on Ice");
-        event.setTotalSpots(100);
-        event.setPartner(new PartnerDTO(disney.getId()));
+        var event = new NewEventDTO("Disney on Ice", "2021-01-01", 100, disney.getId());
 
         final var result = this.mvc.perform(
                         MockMvcRequestBuilders.post("/events")
@@ -75,22 +70,17 @@ class EventControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber())
                 .andReturn().getResponse().getContentAsByteArray();
 
-        var actualResponse = mapper.readValue(result, EventDTO.class);
-        Assertions.assertEquals(event.getDate(), actualResponse.getDate());
-        Assertions.assertEquals(event.getTotalSpots(), actualResponse.getTotalSpots());
-        Assertions.assertEquals(event.getName(), actualResponse.getName());
+        var actualResponse = mapper.readValue(result, NewEventDTO.class);
+        Assertions.assertEquals(event.date(), actualResponse.date());
+        Assertions.assertEquals(event.totalSpots(), actualResponse.totalSpots());
+        Assertions.assertEquals(event.name(), actualResponse.name());
     }
 
     @Test
     @Transactional
     @DisplayName("Deve comprar um ticket de um evento")
     public void testReserveTicket() throws Exception {
-
-        var event = new EventDTO();
-        event.setDate("2021-01-01");
-        event.setName("Disney on Ice");
-        event.setTotalSpots(100);
-        event.setPartner(new PartnerDTO(disney.getId()));
+        var event = new NewEventDTO("Disney on Ice", "2021-01-01", 100, disney.getId());
 
         final var createResult = this.mvc.perform(
                         MockMvcRequestBuilders.post("/events")
@@ -101,10 +91,9 @@ class EventControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber())
                 .andReturn().getResponse().getContentAsByteArray();
 
-        var eventId = mapper.readValue(createResult, EventDTO.class).getId();
+        var eventId = mapper.readValue(createResult, CreateCustomerUseCase.Output.class).id();
 
-        var sub = new SubscribeDTO();
-        sub.setCustomerId(johnDoe.getId());
+        var sub = new SubscribeDTO(johnDoe.getId());
 
         this.mvc.perform(
                         MockMvcRequestBuilders.post("/events/{id}/subscribe", eventId)
