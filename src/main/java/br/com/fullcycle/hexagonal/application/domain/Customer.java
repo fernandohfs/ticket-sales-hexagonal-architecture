9ca816e4b215f38189ad45f831e7feae603ef950
Fernando Soares <fernando.hfs94@gmail.com>
2024-08-13 21:02:02 -0300

    refactor(domain): adjust domain entities setters

--- src/main/java/br/com/fullcycle/hexagonal/application/domain/Customer.java
+++ src/main/java/br/com/fullcycle/hexagonal/application/domain/Customer.java
@@ -15,9 +15,9 @@ public class Customer {
         }
 
         this.customerId = customerId;
-        this.name = new Name(name);
-        this.cpf = new Cpf(cpf);
-        this.email = new Email(email);
+        this.setName(name);
+        this.setCpf(cpf);
+        this.setEmail(email);
     }
 
     public static Customer newCustomer(String name, String cpf, String email) {
@@ -38,5 +38,17 @@ public class Customer {
 
     public Email email() {
         return email;
+    }
+
+    private void setCpf(final String cpf) {
+        this.cpf = new Cpf(cpf);
+    }
+
+    private void setEmail(final String email) {
+        this.email = new Email(email);
+    }
+
+    private void setName(final String name) {
+        this.name = new Name(name);
     }
 }
